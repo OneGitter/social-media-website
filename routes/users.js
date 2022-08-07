4a37aa8e5158b13d4dba250b14616d4687bea878
Naseer <mnu202001@gmail.com>
2022-08-07 17:16:24 +0530

    controllers and routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controllers/user-controller');
+const userController = require('../controllers/users_controller');
 
 router.get('/profile',userController.profile);
 router.get('/',userController.home);
