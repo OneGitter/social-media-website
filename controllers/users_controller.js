373a8c9e194adff78c63135bde016d6aaefe4863
Naseer <mnu202001@gmail.com>
2022-08-11 12:23:04 +0530

    finalized setup for authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function (req,res) {
     res.render('user_profile',{
         title: "Profile"
@@ -16,4 +19,32 @@ module.exports.signIn = function (req,res) {
     return res.render('user_sign_in', {
         title: "Code'nChat | Sign In"
     })
+}
+
+// get the sign up data
+module.exports.create_id = function (req,res) {
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function (err) {
+        if(err) {
+            console.log(`${err} in finding user`);
+            return;
+        }
+        if(!user){
+            User.create(req.body, function (err, user) {
+                if(err) {
+                    console.log(`${err} in creating user`);
+                    return;
+                }
+                
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else {
+            return res.redirect('back');
+        }
+
+    });   
 }
