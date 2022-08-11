d7a61377cd6997d3f958f8a66411771d6e41f8b4
Naseer <mnu202001@gmail.com>
2022-08-11 09:48:42 +0530

    test

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+// use static files
+app.use(express.static('./assets'));
+
 // use express layouts
 app.use(expressLayouts);
 
