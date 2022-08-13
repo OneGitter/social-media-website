f89c1cb9909c0fb3fc03d3809e3b9dd5fa1481ff
Naseer <mnu202001@gmail.com>
2022-08-13 11:33:27 +0530

    added posting feature with authentication enabled

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,15 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req,res) {
-    return res.render('home', {
-        title: "Code'n Chat"
-    });
+
+    Post.find({}).populate('user').exec(function (err,posts) {
+        if(err){
+            console.log(`${err} while fetching posts`);
+            return;
+        }
+        return res.render('home', {
+            title: "Code'n Chat",
+            posts: posts
+        });
+    })
 }
