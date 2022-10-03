1aa1c29ae7383c4db4ae4bceccafc1464192f380
Naseer <mnu202001@gmail.com>
2022-10-03 12:02:18 +0530

    added new strategy passport jwt

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/user');
+
+
+module.exports.createSession = async function (req,res) {
+    try {
+        let user = await User.findOne({email: req.body.email});
+
+        if(!user || user.password !=req.body.password){
+            return res.json(422,{
+                message: "Invalid username or password"
+            });
+        }
+
+        return res.json(200, {
+            message: 'Sign in successful , here is your token',
+            data: {
+                token: jwt.sign(user.toJSON(), 'codenchat', {expiresIn: '100000'})
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return res.json(500, {
+            message: 'Internal Server Error'
+        });
+    }
+}
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local');
+const passportJWT = require('./config/passport-jwt');
 const mongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
